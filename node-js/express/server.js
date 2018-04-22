81d15742fef24ed6171e3cc087dc79b94f412d41
KimJungChul <kjc5443@gmail.com>
2018-04-23 03:50:26 +0900

    express server.js

---
+++ node-js/express/server.js
@@ -0,0 +1,11 @@
+var fs = require('fs');
+var express = require('express');
+
+var app = express();
+app.use(express.static(__dirname+"/public"));
+
+app.get("/", function(request, response) {
+	response.send("Hello");
+});
+
+app.listen(8080, localhost);
